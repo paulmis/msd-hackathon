65e1300b1d03064af3677d317d76dcb66438b068
paulmis <chugdevs@gmail.com>
2023-02-25 20:09:04 +0100

    add list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,24 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Chat from './chat/Chat';
+import List from './list/List';
 
-function App() {
-  return (
-    <div className="app">
-      <div className="header">
-        <img src="logo.png" className="logo"/>
-        <h2>Desearch</h2>
-      </div>
-      <div className="main">
-        <div className="left">
-          <p>text</p>
-        </div>
-        <div className="right">
-          <Chat/>
-        </div>
-      </div>
-    </div>
-  );
+export type ListState = {
+  organizations: Map<string, string>[]
 }
 
-export default App;
+export default class App extends React.Component<{}, ListState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      organizations: []
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="app">
+        <div className="header">
+          <img src="logo.png" className="logo"/>
+          <div className="appTitle">
+            <h2>Desearch</h2>
+            <p>Find the right organizations in Delft</p>
+          </div>
+        </div>
+        <div className="main">
+          <div className="left">
+            <p>text</p>
+          </div>
+          <div className="right">
+            <Chat update_callback={(orgs) => this.setState({organizations: orgs})}/>
+          </div>
+        </div>
+        <List organizations={this.state.organizations}/>
+      </div>
+    );
+  }
+}
---
+++ frontend/src/list/List.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./List.css";
+import Organization from "./Organization";
+
+export type ListProps = {
+    organizations: Map<string, string>[]
+}
+
+export default class List extends React.Component<ListProps> {
+    render() {
+        return <div className="list">
+                {
+                    this.props.organizations.map((entry, index) => {
+                        return <Organization key={index} info={entry} displayFields={["location"]}/>
+                    })
+                }
+            </div>
+    }
+}
